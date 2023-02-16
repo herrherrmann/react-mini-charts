0ac320f82534bd8f2569fac280398a50b3f94a09
Sebastian Herrmann <sebastian@herrherrmann.net>
2023-02-16 18:51:19 +0100

    Add more docs to PieChart TS types

--- src/components/PieChart/types.d.ts
+++ src/components/PieChart/types.d.ts
@@ -1,6 +1,8 @@
 import { Color } from '../../common/types'
 
 export type PieChartItem = {
+	/** Percentage between 0.0 and 1.0. */
 	percentage: number
+	/** Color of the pie slice. */
 	color: Color
 }
