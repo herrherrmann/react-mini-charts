c986b2e9cd3ab897fec43c4b2edd56eff65574d0
Sebastian Herrmann <sebastian@herrherrmann.net>
2023-02-16 16:28:03 +0100

    Replace Spacer with CSS-based margins

--- src/components/BarChart/Item/index.tsx
+++ src/components/BarChart/Item/index.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react'
 import { Color } from '../../../common/types'
-import Spacer from '../../Spacer'
 import Bar, { BarProps } from '../Bar'
 import styles from './styles.module.css'
 
@@ -35,11 +34,12 @@ export default function Item({
 	return (
 		<div className={styles.itemContainer}>
 			<Bar barColor={barColor} barWidth={barWidth} />
-			<Spacer size={6} />
-			<div className={styles.labels}>
-				{labelLeft && <Label color={labelLeftColor}>{labelLeft}</Label>}
-				{labelRight && <Label color={labelRightColor}>{labelRight}</Label>}
-			</div>
+			{(labelLeft || labelRight) && (
+				<div className={styles.labels}>
+					{labelLeft && <Label color={labelLeftColor}>{labelLeft}</Label>}
+					{labelRight && <Label color={labelRightColor}>{labelRight}</Label>}
+				</div>
+			)}
 		</div>
 	)
 }
