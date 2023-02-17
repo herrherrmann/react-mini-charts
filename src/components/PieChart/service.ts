34088e0514a5c59d366e1297be22ede93fc3db71
Sebastian Herrmann <sebastian@herrherrmann.net>
2023-02-17 17:38:29 +0100

    Improve variable name in PieChart service

--- src/components/PieChart/service.ts
+++ src/components/PieChart/service.ts
@@ -3,7 +3,7 @@ import { PieChartItem } from './types'
 export function getOffsetPercentages(items: PieChartItem[]): number[] {
 	return items.map((_item, index) => {
 		const previousItems = items.slice(0, index)
-		const offsetPercentage = previousItems.reduce((prev, current) => prev + current.percentage, 0)
+		const offsetPercentage = previousItems.reduce((sum, item) => sum + item.percentage, 0)
 		return offsetPercentage
 	})
 }
