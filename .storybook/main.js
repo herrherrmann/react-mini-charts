fba1bc757486d9d5c80f076ddea461edd5864f7f
Sebastian Herrmann <sebastian@herrherrmann.net>
2023-02-17 17:24:47 +0100

    Reformat Storybook files

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,6 @@
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-	addons: [
-		'@storybook/addon-essentials',
-		'@storybook/preset-create-react-app',
-	],
+	addons: ['@storybook/addon-essentials', '@storybook/preset-create-react-app'],
 	framework: '@storybook/react',
 	core: {
 		builder: '@storybook/builder-webpack5',
